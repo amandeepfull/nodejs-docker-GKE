51fee9dbf32e6e97b46b0ab10e42945f77e89d6c
Amandeep <amandeep.santokh@full.co>
2019-04-01 11:35:17 +0530

    code optimized

--- src/react/actions/types.js
+++ src/react/actions/types.js
@@ -1,6 +1,6 @@
 export const UPDATE_CONTACT = 'UPDATE_CONTACT';
 export const FETCH_CONTACTS = 'FETCH_CONTACTS';
 export const CONTACT_DELETE_MSG_VIEW = 'CONTACT_DELETE_MSG_VIEW';
-//common method to construct payload
+
 
 

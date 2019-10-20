1e59185905207f10af5dba6c828272f3039f220a
Amandeep <amandeep.santokh@full.co>
2019-10-21 02:54:36 +0530

    added test cases

--- test/usertest.js
+++ test/usertest.js
@@ -1,22 +1,9 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-var should = chai.should();
-chai.use(chaiHttp);
-let server = require('../src/routes/api/user');
-console.log("server : ",server);
-
-//Our parent block
-describe('Contacts', () => {
- describe('/GET contacts', () => {
-     it('it should GET all the contacts', (done) => {
-     chai.request(server)
-       .get('/api/v1/user/contacts')
-       .end((err, res) => {
-             (res).should.have.status(200);
-             (res.body).should.be.a('object');
-             (res.body.users.length).should.be.eql(1);
-             done();
-          });
-       });
-  });
+var assert = require('assert');
+describe('Basic Mocha String Test', function () {
+ it('should return number of charachters in a string', function () {
+        assert.equal("Hello".length, 5);
+    });
+ it('should return first charachter of the string', function () {
+        assert.equal("Hello".charAt(0), 'H');
+    });
 });
